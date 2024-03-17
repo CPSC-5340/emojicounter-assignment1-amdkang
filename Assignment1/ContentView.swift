189c565270de6eb982b4f16e04e83426d0119c19
Amenda Kang <amendakang@gmail.com>
2024-03-17 07:21:37 -0400

    updated emojicounter

--- Assignment1/ContentView.swift
+++ Assignment1/ContentView.swift
@@ -7,20 +7,63 @@
 
 import SwiftUI
 
+let emojis: [String] = ["🤣","😜","😀","🤩","😂"]
+
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            List {
+                ForEach(emojis, id: \.self ) {
+                    emoji in ListItem(emoji: emoji)
+                }
+            }
+            .listStyle(.grouped)
+            .navigationTitle("Emoji Counter")
+            .navigationBarTitleDisplayMode(.inline)
         }
-        .padding()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct ListItem: View {
+    var emoji: String
+    @State var counter = 0
+    
+    var body: some View {
+        HStack {
+            Text(emoji)
+            Text("Counter:  \(counter)")
+                .foregroundColor(.red)
+            Spacer()
+            Image(systemName: "plus")
+                .imageScale(.large)
+                .padding(3)
+                .onTapGesture {
+                    counter += 1
+                }
+                .overlay(RoundedRectangle(cornerRadius: 8)
+                    .stroke(Color.red)
+                )
+                .foregroundColor(.red)
+                .padding()
+            
+            Image(systemName: "minus")
+                .imageScale(.large)
+                .padding(9)
+                .onTapGesture {
+                    counter -= 1
+                }
+                .overlay(RoundedRectangle(cornerRadius: 8)
+                    .stroke(Color.red)
+                )
+                .foregroundColor(.red)
+        }
+        .foregroundColor(.white)
+        .padding()
     }
 }
